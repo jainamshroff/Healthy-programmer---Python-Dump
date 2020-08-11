93fb4ca863e21fea2620154673dade0ae99e73ea
Jainam Shroff <jainam.shroff@outlook.com>
2020-08-12 00:37:58 +0530

    Add files via upload

---
+++ Healthy Programmer/main.py
@@ -0,0 +1,15 @@
+# Healthy Programmer Exercise
+
+# Water - water.mp3 -  every 45 mins - "wadone" - command to stop - file will log with date and time
+# Eyes - eyes.mp3 - every 30 mins - "eydone" - command to stop -  file will log with date and time
+# Physical Activity - physical.mp3 -  every 45 mins - "exdone" - command to stop - file will log with date and time
+
+# What we have to do is that we have to make the user that from 9 am to 5 pm the user
+# should drink 3.5 L of water, we have to play water.mp3 at every certain time to stop the
+# alarm the user should enter "drank" command to stop the sound, similarly in each 30 min
+# the eye mp3 should round and user should enter "done" to reset, and again the timer
+# should start, similarly for exercise
+
+# Module pygame - to play audio
+# date time module - to work with date and time
+
